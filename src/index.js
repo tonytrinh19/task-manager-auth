7085443e69aa9cc37136b6b8a713b3d14d1f95e2
tonytrinh19 <tony.q.trinh@gmail.com>
2021-12-15 15:58:57 -0800

    Added upload avatar feature for user. Need auth next.

--- src/index.js
+++ src/index.js
@@ -7,28 +7,13 @@ const taskRouter = require('./routers/task-router.js')
 
 const port = process.env.PORT || 3000
 
-// Express middleware
-// app.use((req, res, next) => {
-//     res.status(503).send('Server is under maintenance! Please return at a different time.')
-// })
-
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
 
 app.listen(port, () => console.log('Server is up on port ', port))
 
-const User = require('./models/user')
-const Task = require('./models/task.js')
-
-const main = async () => {
-    // const user = await User.findById('60ef2a0e5514461c8c3e5939')
-    // await user.populate('tasks').execPopulate()
-    // console.log(user.tasks)
-    
-    // const task = await Task.findById('60b7a27b13caad21dc0beb4a')
-    // await task.populate('creator').execPopulate()
-    // console.log(task.creator)
-}
-
-main()
+// Express middleware
+// app.use((req, res, next) => {
+//     res.status(503).send('Server is under maintenance! Please return at a different time.')
+// })
--- src/routers/user-router.js
+++ src/routers/user-router.js
@@ -4,6 +4,8 @@ const router = new express.Router()
 const User = require('../models/user')
 const auth = require('../middleware/auth')
 const { update } = require('../models/user')
+const multer = require('multer')
+const upload = multer({ dest: 'avatars'})
 
 router.post('/users', async (req, res) => {
     const user = new User(req.body)
@@ -105,6 +107,11 @@ router.delete('/users/me', auth, async (req, res) => {
     } catch (error) {
         res.status(500).send()
     }
+})
+
+router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
+
+    res.send()
 })
 
 module.exports = router
