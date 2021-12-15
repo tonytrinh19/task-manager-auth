9abc044e9c4f9ec5bd7c71c3a8457216dc713057
tonytrinh19 <tony.q.trinh@gmail.com>
2021-12-15 14:59:08 -0800

    Added sort params to tasks.

--- src/routers/task-router.js
+++ src/routers/task-router.js
@@ -23,17 +23,23 @@ router.post('/tasks', auth, async (req, res) => {
 
 // GET /tasks?completed=true. Add filtering to the request pararms.
 // GET /tasks?limit=10&skip=10. Limit: number of queries per page, skip: number of queries skipped.
+// GET /tasks/sortBy=createdAt:asc. Sort params.
 router.get('/tasks', auth, async (req, res) => {
     try {
         // authenticate the user logged in, only show tasks that belong to currently logged in user
         // const tasks = await Task.find({creator: req.user._id})
 
         const match = {}
+        const sort = {}
 
         if (req.query.completed) {
             match.completed = req.query.completed === 'true'
         }
 
+        if (req.query.sortBy) {
+            const strings = req.query.sortBy.split(':')
+            sort.strings[0] = strings[1] === 'asc' ? 1 : -1 
+        }
 
         await req.user.populate({
             path: 'tasks',
@@ -41,6 +47,7 @@ router.get('/tasks', auth, async (req, res) => {
             options: {
                 limit: parseInt(req.query.limit),
                 skip: parseInt(req.query.skip),
+                sort,
             },
         }).execPopulate()
         res.status(200).send(req.user.tasks)
