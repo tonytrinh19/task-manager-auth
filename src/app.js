23073affd59b752618a345f21ca7fe92582f91a4
tonytrinh19 <tony.q.trinh@gmail.com>
2021-12-31 15:06:01 -0800

    Made some minor changes.

--- src/app.js
+++ src/app.js
@@ -16,6 +16,7 @@ const userRouter = require('./routers/user-router')
 const taskRouter = require('./routers/task-router')
 
 app.use(express.json())
+app.use(express.urlencoded())
 app.use(appRouter)
 app.use(userRouter)
 app.use(taskRouter)
