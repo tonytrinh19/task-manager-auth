82d1be96452bce9214f64700ab22c9c60f504671
tonytrinh19 <tony.q.trinh@gmail.com>
2022-01-02 20:52:55 -0800

    Changed expire time for json web token to 1 hour.

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -5,6 +5,7 @@ const auth = async (req, res, next) => {
     try {
         const token = req.header('Authorization').replace('Bearer ', '')
         const decoded = jwt.verify(token, process.env.JWT_KEY)
+        if (!decoded) throw new Error()
         const user = await User.findOne({
             _id: decoded._id,
             'tokens.token': token
